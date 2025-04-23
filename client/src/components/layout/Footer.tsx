91a548aa3f2300c5d6727e2f3a2e8d942c2e5adc
Dilgo-dev <dilgopierre@gmail.com>
2025-04-23 16:04:04 +0200

    fix(client/login): Change language into French

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -1,7 +1,3 @@
 export default function Footer() {
-  return (
-    <footer className="py-4 text-sm text-gray-600">
-      Made by CareTech with <span className="text-red-500">❤</span>
-    </footer>
-  );
+  return <footer className="py-4 text-sm text-gray-600">Fait avec ❤️ par CareTech</footer>;
 }
