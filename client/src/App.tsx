b15f6e0eead16524b030102b4f1e94baa9e9d139
Flam64 <celio.mozes.rocha@gmail.com>
2025-04-24 16:45:47 +0200

    feat(client/PageNotFound): Add page 404

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from '@/components/layout/Layout';
 import HomePage from '@/pages/Home';
 import Login from '@/pages/Login';
-import PageNotFound from './pages/PageNotFound';
+import PageNotFound from '@/pages/PageNotFound';
 
 const router = createBrowserRouter([
   {
