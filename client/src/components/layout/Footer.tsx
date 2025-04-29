f0dac636e25cc2430edc4a953ca7b8334706ad79
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-04-29 22:32:02 +0200

    feat(style/logo): careTech logo added

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -1,3 +1,9 @@
 export default function Footer() {
-  return <footer className="py-4 text-sm text-blue">Fait avec ❤️ par CareTech</footer>;
+  return (
+    <footer className="flex items-center justify-center gap-1 py-4 text-sm text-blue">
+      Fait avec
+      <img src="/public/heart-handshake.svg" alt="logo careTech" className="w-6 inline-block" />
+      par CareTech
+    </footer>
+  );
 }
