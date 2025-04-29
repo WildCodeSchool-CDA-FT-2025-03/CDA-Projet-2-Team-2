11ad1fa36b43b3e3dc0e3cc09d35216cb2a122d9
Dilgo-dev <dilgopierre@gmail.com>
2025-04-29 17:47:34 +0200

    feat(server/database/client): Add log table into client

--- server/src/database/client.ts
+++ server/src/database/client.ts
@@ -5,6 +5,7 @@ import { Planning } from '../entities/planning.entity';
 import { Departement } from '../entities/departement.entity';
 import { City } from '../entities/city.entity';
 import { Patient } from '../entities/patient.entity';
+import { Log } from '../entities/log.entity';
 
 import 'dotenv/config';
 
@@ -15,7 +16,7 @@ export const dataSource = new DataSource({
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_DATABASE || 'doctoplan',
-  entities: [User, Planning, Departement, City, Patient],
+  entities: [User, Planning, Departement, City, Patient, Log],
   synchronize: true,
   logging: true,
 });
