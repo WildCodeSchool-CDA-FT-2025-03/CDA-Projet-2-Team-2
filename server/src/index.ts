c2182066bb3df6cfef9d0924061272dafacdaa08
Dilgo-dev <dilgopierre@gmail.com>
2025-04-29 14:55:05 +0200

    refactor(*/hello): Remove boilerplate code

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,6 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import { buildSchema } from 'type-graphql';
 
 import { dataSource } from './database/client';
-import { HelloResolver } from './resolvers/hello.resolver';
 import { AuthResolver } from './resolvers/auth.resolver';
 
 import 'dotenv/config';
@@ -13,7 +12,7 @@ async function startServer() {
   await dataSource.initialize();
 
   const schema = await buildSchema({
-    resolvers: [HelloResolver, AuthResolver],
+    resolvers: [AuthResolver],
   });
 
   const server = new ApolloServer({
