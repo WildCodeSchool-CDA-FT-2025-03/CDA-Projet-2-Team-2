2c0ebcf5845bca40455ae98059f18123c51a7c82
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-04-30 16:38:39 +0200

    feat(nextRdv/style): fakeData added for next rdv

--- client/src/pages/patientFile/ModuleList.tsx
+++ client/src/pages/patientFile/ModuleList.tsx
@@ -1,42 +1,19 @@
-const data = [
-  {
-    doctor: 'Dr Bishop',
-    specialty: 'Dermatologie',
-    date: '24/04/2025',
-    time: '11h05',
-  },
-  {
-    doctor: 'Dr Maboul',
-    specialty: 'Dermatologie',
-    date: '24/04/2025',
-    time: '11h30',
-  },
-  {
-    doctor: 'Dr Who',
-    specialty: 'Psychiatrie',
-    date: '29/04/2025',
-    time: '15h50',
-  },
-  {
-    doctor: 'Dr House',
-    specialty: 'Urologie',
-    date: '01/07/2025',
-    time: '18h30',
-  },
-];
+// Définition du type des props du composant avec une contrainte sur T
+type ModuleListProps<T extends { id: string | number }> = {
+  data: T[];
+  renderItem: (item: T, index: number) => React.ReactNode;
+};
 
-export default function ModuleList() {
+export default function ModuleList<T extends { id: string | number }>({
+  data,
+  renderItem,
+}: ModuleListProps<T>) {
   return (
     <ul className="border-l-4 border-blue rounded-md shadow-sm divide-y divide-gray-100 w-full max-w-md mx-auto bg-white space-y-2">
       {data.map((item, index) => (
-        <li
-          key={item.date}
-          className={`px-4 py-3 ${index % 2 === 0 ? 'bg-white' : 'bg-lightBlue'}`}
-        >
-          <span className="font-bold text-gray-800">{item.doctor}</span>
-          <span className="text-gray-600">
-            - {item.specialty} - {item.date} - {item.time}
-          </span>
+        <li key={item.id} className={`px-4 py-3 ${index % 2 === 0 ? 'bg-white' : 'bg-[#E9F2FF]'}`}>
+          {/* La fonction "renderItem" permet de personnaliser l'affichage de chaque élément */}
+          {renderItem(item, index)}
         </li>
       ))}
     </ul>
