aec31e7831896df938469620fff634f2a90c2487
Dilgo-dev <dilgopierre@gmail.com>
2025-05-07 15:12:06 +0200

    feat(server/scalar/json): Add custom scalar for log metadata

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -4,11 +4,13 @@ import { PatientResolver } from './resolvers/patient.resolver';
 import { AuthResolver } from './resolvers/auth.resolver';
 import { DepartementResolver } from './resolvers/departement.resolver';
 import { CityResolver } from './resolvers/city.resolver';
+import { LogResolver } from './resolvers/log.resolver';
 import { getUserFromToken } from './utils/jwt.utils';
+import { JSONScalar } from './scalar/json.scalar';
 
 export default async function createSchema() {
   return await buildSchema({
-    resolvers: [AuthResolver, DepartementResolver, PatientResolver, CityResolver],
+    resolvers: [AuthResolver, DepartementResolver, PatientResolver, CityResolver, LogResolver],
     authChecker: async ({ context }, roles) => {
       const user = await getUserFromToken(context.req.headers.cookie);
 
@@ -18,5 +20,11 @@ export default async function createSchema() {
 
       return roles.includes(user.role);
     },
+    scalarsMap: [
+      {
+        type: Object,
+        scalar: JSONScalar,
+      },
+    ],
   });
 }
