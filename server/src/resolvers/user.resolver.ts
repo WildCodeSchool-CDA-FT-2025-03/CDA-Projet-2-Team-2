b7e6224d7af5d57aeaa4d29ef4fd7d33cd319991
abdifatah <abdifatah4@msn.com>
2025-05-13 10:27:59 +0200

    feat(server/user): create user

--- server/src/resolvers/user.resolver.ts
+++ server/src/resolvers/user.resolver.ts
@@ -1,10 +1,42 @@
-import { Query, Resolver } from 'type-graphql';
-import { User } from '../entities/user.entity';
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { User, UserRole, UserStatus } from '../entities/user.entity';
+import { CreateUserInput } from '../types/user.type';
+import { GraphQLError } from 'graphql';
+import { Departement } from '../entities/departement.entity';
 
 @Resolver()
 export class UserResolver {
   @Query(() => [User])
   async getAllUsers() {
     return await User.find({ relations: ['departement'] });
+  }
+
+  @Mutation(() => Boolean)
+  async createUser(@Arg('data') data: CreateUserInput) {
+    const departement = await Departement.findOneBy({ id: data.departementId });
+    const userExist = await User.findOneBy({ email: data.email });
+
+    if (userExist) {
+      throw new GraphQLError("Échec de la création de l'utilisateur", {
+        extensions: {
+          code: 'USER_CREATION_FAILED',
+          originalError: "l'utilisateur existe déjà",
+        },
+      });
+    }
+
+    const newUser = new User();
+    newUser.email = data.email;
+    newUser.password = '';
+    newUser.firstname = data.firstname;
+    newUser.lastname = data.lastname;
+    newUser.role = data.role as UserRole;
+    newUser.status = data.status as UserStatus;
+    if (departement) {
+      newUser.departement = departement;
+    }
+
+    await newUser.save();
+    return true;
   }
 }
