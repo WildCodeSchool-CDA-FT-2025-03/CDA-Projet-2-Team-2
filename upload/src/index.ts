70f71742f643dc34c001e2ec5e8fa469a4cf2df8
Alexandre DUMOUT <alexdum03@hotmail.fr>
2025-05-20 12:20:31 +0200

    feat(upload/server): Ajout server express upload

---
+++ upload/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import router from "./router";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(router);
+
+const port = process.env.SERVER_PORT || 5050;
+
+app.listen(port, () => {
+  console.log(`Server to manage upload is running on http://localhost:${port}`);
+});
