c678d761c4acce78a61c3b67bc36f6bad2a08ad4
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-05-21 17:09:38 +0200

    feat(componentTest/test): basic test on Header to verify set up is working

---
+++ client/src/components/layout/Header.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import Header from './Header';
+import { expect, it } from 'vitest';
+
+it('le header affiche le logo DoctoPlan', () => {
+  render(<Header />);
+
+  // Vérifie que l'image du logo est bien présente
+  expect(screen.getByAltText('logo de DoctoPlan')).toBeInTheDocument();
+
+  // Vérifie aussi que le texte DoctoPlan est affiché
+  expect(screen.getByText('DoctoPlan')).toBeInTheDocument();
+});
