421cdd65cff379521e21e30448b02c12839bf98e
Dilgo-dev <dilgopierre@gmail.com>
2025-05-22 17:37:01 +0200

    refactor(server): update server response message and change port to 3333

--- logs/cmd/server/main.go
+++ logs/cmd/server/main.go
@@ -1,16 +1,11 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/gin-gonic/gin"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello, World!")
 	server := gin.Default()
 	server.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Hello, World!"})
+		c.JSON(200, gin.H{"message": "Hello, World!!"})
 	})
-	server.Run(":8080")
+	server.Run(":3333")
 }
