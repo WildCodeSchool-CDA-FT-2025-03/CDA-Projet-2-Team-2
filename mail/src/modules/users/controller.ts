f17a7e59b75be289966f34f6012fdd66fe3874bb
Flam64 <celio.mozes.rocha@gmail.com>
2025-05-22 17:35:20 +0200

    feat(client/forgotPassword): (added) password reset via the mail microservice

--- mail/src/modules/users/controller.ts
+++ mail/src/modules/users/controller.ts
@@ -4,7 +4,7 @@ import { resetPassword } from '../../utils/mails/reset.mail';
 const sendResetPassword: RequestHandler = async (req, res, next) => {
   const { email, url } = req.body;
   try {
-    //await resetPassword(email, url);
+    await resetPassword(email, url);
     res.sendStatus(200);
   } catch (error) {
     next(error);
