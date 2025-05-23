dd403a81ee09afddd6ccbcc31b1b68fc2ae77b16
Dilgo-dev <dilgopierre@gmail.com>
2025-05-23 09:42:39 +0200

    feat(models): update Log model to include ID, Titre, Metadata, and CreateAt fields

--- logs/internal/models/log.go
+++ logs/internal/models/log.go
@@ -1,8 +1,14 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+)
 
 type Log struct {
-	gorm.Model
-	Message string
+	ID       string    `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+	Titre    string    `gorm:"size:250"`
+	Metadata JSON      `gorm:"type:jsonb;nullable"`
+	CreateAt time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP;column:create_at"`
 }
+
+type JSON map[string]interface{}
