3a8186b82d61eb5226dde7fc41fb57219cf631f0
Dilgo-dev <dilgopierre@gmail.com>
2025-05-23 11:03:41 +0200

    refactor(models): change Metadata field type to use gorm datatypes.JSON for improved JSON handling

--- logs/internal/models/log.go
+++ logs/internal/models/log.go
@@ -1,11 +1,14 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/datatypes"
+	"gorm.io/gorm"
+)
 
 type Log struct {
 	gorm.Model
-	Titre    string `gorm:"size:250"`
-	Metadata JSON   `gorm:"type:jsonb;nullable"`
+	Titre    string         `gorm:"size:250"`
+	Metadata datatypes.JSON `gorm:"type:jsonb;nullable"`
 }
 
 type JSON map[string]interface{}
