1395dbc97692c6bca1fa796470a81dcb3df9393b
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-05-28 11:49:39 +0200

    feat(createAppointement/doctorRdvPage): basic style done

--- client/src/pages/NewAppointementByDoctor.tsx
+++ client/src/pages/NewAppointementByDoctor.tsx
@@ -7,13 +7,20 @@ export default function NewAppointementByDoctor() {
   return (
     <>
       <div>{`NewAppointementByDoctor ${params.get('doctor')}`}</div>
-      <h2>
-        Creer un rendez-vous avec Nom du doctor, <span>profession, service</span>
-      </h2>
-      <section>
-        <SearchBar />
-      </section>
-      <section>
+      <div className="flex flex-col w-3/4">
+        <section className="flex flex-col gap-4 self-start">
+          <div className="flex gap-4">
+            <img src="/calendar-clock.svg" alt="icone de creation de rendez-vous" />
+            <h2>
+              Creer un rendez-vous avec Nom du doctor, <span>profession, service</span>
+            </h2>
+          </div>
+          <div className="self-start">
+            <SearchBar />
+          </div>
+        </section>
+      </div>
+      <section className="bg-bgBodyColor sm:w-full md:w-3/4 p-4 sm:p-6 md:p-12 lg:p-24 rounded-sm shadow-md border-borderColor flex flex-col md:flex-row justify-center gap-10 md:gap-45">
         <aside>
           <DayPilotNavigator />
         </aside>
@@ -22,8 +29,8 @@ export default function NewAppointementByDoctor() {
           <h3>Select motif de consultation</h3>
           <div>
             Debut - Fin
-            <button className="cta-red">Annuler</button>
-            <button className="cta">Enregistrer</button>
+            <button className="standard-button-red">Annuler</button>
+            <button className="standard-button">Enregistrer</button>
           </div>
         </div>
       </section>
