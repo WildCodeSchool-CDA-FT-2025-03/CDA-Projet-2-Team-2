1bae45bba1c4e2b88f1bf293aeeb0e6945c05f96
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-05-28 10:46:31 +0200

    feat(createAppointement/secretaryPage): tsructue create appointement via doctor structure

--- client/src/pages/NewAppointementByDoctor.tsx
+++ client/src/pages/NewAppointementByDoctor.tsx
@@ -1,6 +1,32 @@
+import SearchBar from '@/components/form/SearchBar';
+import { DayPilotNavigator } from '@daypilot/daypilot-lite-react';
 import { useSearchParams } from 'react-router-dom';
 
 export default function NewAppointementByDoctor() {
   const [params] = useSearchParams();
-  return <div>{`NewAppointementByDoctor ${params.get('doctor')}`}</div>;
+  return (
+    <>
+      <div>{`NewAppointementByDoctor ${params.get('doctor')}`}</div>
+      <h2>
+        Creer un rendez-vous avec Nom du doctor, <span>profession, service</span>
+      </h2>
+      <section>
+        <SearchBar />
+      </section>
+      <section>
+        <aside>
+          <DayPilotNavigator />
+        </aside>
+        <div>
+          <h3>Patient info</h3>
+          <h3>Select motif de consultation</h3>
+          <div>
+            Debut - Fin
+            <button className="cta-red">Annuler</button>
+            <button className="cta">Enregistrer</button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
