54580c9008a1c73dc85bc97f9bfb57754db9e594
Alexandre DUMOUT <alexdum03@hotmail.fr>
2025-06-06 14:29:46 +0200

    feat(server/appointment): Modification mutation création rendez-vous

--- server/src/types/appointment.type.ts
+++ server/src/types/appointment.type.ts
@@ -1,32 +1,47 @@
 import { Field, InputType } from 'type-graphql';
+import { Matches, IsNumber, IsUUID } from 'class-validator';
 import { AppointmentStatus } from '../entities/appointment.entity';
 
 @InputType()
 export class AppointmentCreateInput {
   @Field()
-  id?: number;
+  @Matches(/^(\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])T([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/, {
+    message: 'La date doit être au format yyyy-mm-ddThh:mm:ss avec des valeurs valides.',
+  })
+  start_time: string; // Date and start hour
 
   @Field()
-  start_time: Date; // Date and start hour
-
-  @Field()
+  @IsNumber()
   duration: number; // in minutes
 
   @Field(() => String, { nullable: true })
   status?: AppointmentStatus;
 
   @Field()
+  @Matches(/^[0-9]{1,10}$/, {
+    message: "L'ID du médecin doit être un nombre valide.",
+  })
   user_id: string; // Doctor ID
 
   @Field()
+  @IsUUID()
   patient_id: string;
 
   @Field()
+  @Matches(/^[0-9]{1,10}$/, {
+    message: "L'ID doit être un nombre valide.",
+  })
   created_by: string;
 
   @Field()
+  @Matches(/^[0-9]{1,10}$/, {
+    message: 'Le type de rendez-vous doit être un nombre valide.',
+  })
   appointmentType: string;
 
   @Field()
+  @Matches(/^[0-9]{1,10}$/, {
+    message: "L'ID du service doit être un nombre valide.",
+  })
   departement: string;
 }
