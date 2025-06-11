8a81d7bf9d8055d9841a02980805c820423b0d04
abdifatah <abdifatah4@msn.com>
2025-06-11 14:53:54 +0200

    feat(server/client/department): pagination added to the department page

--- server/src/resolvers/departement.resolver.ts
+++ server/src/resolvers/departement.resolver.ts
@@ -1,9 +1,10 @@
-import { Arg, Mutation, Query, Resolver, Authorized } from 'type-graphql';
+import { Arg, Mutation, Query, Resolver, Authorized, Int } from 'type-graphql';
 import { Departement, DepartementStatus } from '../entities/departement.entity';
 import { GraphQLError } from 'graphql';
-import { DepartementInput } from '../types/departement.type';
+import { DepartementInput, DepartementsWithTotal } from '../types/departement.type';
 import { UserRole } from '../entities/user.entity';
 import redisClient from '../database/redis';
+import { ILike } from 'typeorm';
 
 @Resolver()
 export class DepartementResolver {
@@ -21,6 +22,24 @@ export class DepartementResolver {
     });
     redisClient.set('departements', JSON.stringify(departements), { EX: 60 * 60 * 24 * 30 });
     return departements;
+  }
+
+  @Query(() => DepartementsWithTotal)
+  @Authorized([UserRole.SECRETARY, UserRole.DOCTOR, UserRole.ADMIN, UserRole.AGENT])
+  async getAllDepartementsWithPagination(
+    @Arg('limit', () => Int, { nullable: true }) limit?: number,
+    @Arg('page', () => Int, { nullable: true }) page?: number,
+    @Arg('search', { nullable: true }) search?: string,
+  ) {
+    const take = limit ?? 0;
+    const skip = page && page > 0 ? (page - 1) * take : 0;
+    const [departements, total] = await Departement.findAndCount({
+      order: { label: 'ASC' },
+      take,
+      skip,
+      where: [{ label: ILike(`%${search}%`) }],
+    });
+    return { departements, total };
   }
 
   @Mutation(() => Boolean)
