a1c89983df4e1657c8cdc5496fa9255f36710328
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-06-12 17:24:35 +0200

    chore(refacto/AgendaWithNavigator): hook for events handlers

--- client/src/components/calendar/AgendaWithNavigator.tsx
+++ client/src/components/calendar/AgendaWithNavigator.tsx
@@ -8,14 +8,13 @@ import useResources from '@/hooks/useResources';
 import useSyncAgendaWithLegalLimit from '@/hooks/useSyncAgendaWithLegalLimit';
 import { useAppointmentContext } from '@/hooks/useAppointment';
 
-import type { Appointment } from '@/types/CalendarEvent.type';
-
 import AgendaHeader from './AgendaHeader';
 import AgendaPagination from './AgendaPagination';
 import AgendaCalendar from './AgendaCalendar';
 import AgendaDateNavigator from './AgendaDateNavigator';
 import ConfirmationModal from '../modals/ConfirmationModal';
 import useSearchSources from '@/hooks/useSearchSources';
+import useAgendaEventHandlers from '@/hooks/useAgendaEventHandlers';
 
 export default function AgendaWithNavigator() {
   const DEFAULT_DEPARTMENT = '1';
@@ -64,43 +63,18 @@ export default function AgendaWithNavigator() {
 
   const searchSources = useSearchSources(searchQuery);
 
-  function handleEventClick(args: { e: { data: Appointment } }) {
-    const event = args.e.data;
-    setModalContent({
-      title: 'Modifier le rendez-vous',
-      message: `Voulez-vous modifier le rendez-vous de ${event.patient_name} ?`,
-      onConfirm: () => navigate('/secretary'),
-    });
-    setModalOpen(true);
-  }
+  const { handleEventClick, handleTimeRangeSelected } = useAgendaEventHandlers({
+    onModalOpen: setModalContentAndOpen,
+    navigate,
+    limitDate: DayPilot.Date.today().addMonths(3),
+  });
 
-  function handleTimeRangeSelected(args: {
-    start: DayPilot.Date;
-    end: DayPilot.Date;
-    resource: string | number;
+  function setModalContentAndOpen(content: {
+    title: string;
+    message: string;
+    onConfirm: () => void;
   }) {
-    const selectedDate = args.start;
-    const today = DayPilot.Date.today();
-    const threeMonthsLater = today.addMonths(3);
-
-    if (selectedDate > threeMonthsLater) {
-      setModalContent({
-        title: 'Date non disponible',
-        message: `Les rendez-vous ne peuvent pas être créés après le ${threeMonthsLater.toString('dd/MM/yyyy')}.`,
-        onConfirm: () => handleDateSelectionWithLimit(today),
-      });
-      setModalOpen(true);
-      return;
-    }
-
-    const doctorId = args.resource;
-    const date = selectedDate.toString();
-
-    setModalContent({
-      title: 'Créer un rendez-vous',
-      message: `Souhaitez-vous créer un rendez-vous le ${date.slice(0, 16).replace('T', ' à ')} ?`,
-      onConfirm: () => navigate(`/secretary/doctor/${doctorId}/appointment/create?date=${date}`),
-    });
+    setModalContent(content);
     setModalOpen(true);
   }
 
