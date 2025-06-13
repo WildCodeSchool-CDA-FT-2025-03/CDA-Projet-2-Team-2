c4a7e0b6dd075d0e8be2654ad2aec81f546e4220
Dilgo-dev <dilgopierre@gmail.com>
2025-06-13 09:33:51 +0200

    fix(client/logo): update logo image path to use direct URL

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -1,10 +1,8 @@
-import doctoplanLogo from '@/assets/doctoplan-logo.svg';
-
 export default function Logo() {
   return (
     <div className="flex items-center">
       <div className="flex items-center justify-center w-16 h-16">
-        <img src={doctoplanLogo} alt="logo de DoctoPlan" />
+        <img src="/doctoplan-logo.svg" alt="logo de DoctoPlan" />
       </div>
       <h1 className="ml-2">DoctoPlan</h1>
     </div>
