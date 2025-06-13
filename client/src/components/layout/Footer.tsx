e56145ebcf3dcc5367f2829a36c906c894d34d0d
Dilgo-dev <dilgopierre@gmail.com>
2025-06-13 09:28:49 +0200

    fix(client/footer): update footer logo image import for correct loading

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -1,8 +1,10 @@
+import heartHandshake from '@/assets/heart-handshake.svg';
+
 export default function Footer() {
   return (
     <footer className="flex items-center justify-center gap-1 py-4 text-sm text-blue">
       Fait avec
-      <img src="/heart-handshake.svg" alt="logo careTech" className="w-6 inline-block" />
+      <img src={heartHandshake} alt="logo careTech" className="w-6 inline-block" />
       par CareTech
     </footer>
   );
