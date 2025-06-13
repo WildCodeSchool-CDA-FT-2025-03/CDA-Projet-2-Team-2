4c82cc9ba59eee501fb4fc992df1dfd2933037de
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-06-13 12:28:59 +0200

    feat(DoctorAgenda/Content): basic style layout

--- client/src/pages/DoctorAgendaPage.tsx
+++ client/src/pages/DoctorAgendaPage.tsx
@@ -9,9 +9,13 @@ export default function DoctorAgendaPage() {
   const doctorId = id ? Number(id) : user?.id;
 
   return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold">Planning du médecin</h1>
-      <p>ID médecin : {doctorId}</p>
+    <div className="py-6 px-6 md:px-24" role="region" aria-label="Agenda hebdomadaire du médecin">
+      <h1 className="text-2xl font-bold mb-4">Planning du médecin</h1>
+
+      <section className="flex flex-col lg:flex-row gap-10 mt-6">
+        {/* On ajoutera le navigator et le calendrier ici dans les étapes suivantes */}
+        <p>ID du médecin : {doctorId}</p>
+      </section>
     </div>
   );
 }
